fa8621cc8d08dfaefbeafea08bf16a96a06b9c47
hyeseung12 <peace10200khs@gmail.com>
2024-04-11 12:10:02 +0900

    #2 헬로월드 출력

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -10,7 +10,7 @@ namespace CSBasic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World?!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
